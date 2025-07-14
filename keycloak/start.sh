530445dbb6000b5f5fc7257aff16cf8bcf628610
DivDE <divde@musicociel.fr>
2025-07-14 17:35:40 +0200

    Update dependencies

--- keycloak/start.sh
+++ keycloak/start.sh
@@ -2,4 +2,4 @@
 
 echo '{}' > ./config.json
 chmod ugo=rw ./config.json
-podman run --rm -p 8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin -v ./config.json:/config.json:rw -v ./profile.json:/profile.json:ro -v ./healthcheck.sh:/healthcheck.sh:ro -v ./entrypoint.sh:/entrypoint.sh:ro --entrypoint /entrypoint.sh quay.io/keycloak/keycloak:26.2.4 start-dev --health-enabled=true 2>&1 | tee keycloak.log
+podman run --rm -p 8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin -v ./config.json:/config.json:rw -v ./profile.json:/profile.json:ro -v ./healthcheck.sh:/healthcheck.sh:ro -v ./entrypoint.sh:/entrypoint.sh:ro --entrypoint /entrypoint.sh quay.io/keycloak/keycloak:26.3.1 start-dev --health-enabled=true 2>&1 | tee keycloak.log
